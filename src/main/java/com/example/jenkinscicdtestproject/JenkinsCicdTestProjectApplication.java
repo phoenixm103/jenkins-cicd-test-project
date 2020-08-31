6321f733936e9a33ca5425277a934671ab3802fc
phoenixm103 <moinuddinm103@gmail.com>
2020-08-31 13:30:49 +0600

    M

--- src/main/java/com/example/jenkinscicdtestproject/JenkinsCicdTestProjectApplication.java
+++ src/main/java/com/example/jenkinscicdtestproject/JenkinsCicdTestProjectApplication.java
@@ -8,6 +8,6 @@ public class JenkinsCicdTestProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsCicdTestProjectApplication.class, args);
-	}
+	}////////
 
 }
